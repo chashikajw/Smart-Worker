3bc730c8cecd5cf10fc1d53d7152fef53554aa08
chashikajw <chashikajw007@gmail.com>
2018-05-23 02:25:08 +0530

    add welcome activity

---
+++ app/src/main/java/dreamso/smart_worker/WelcomeActivity.java
@@ -0,0 +1,25 @@
+package dreamso.smart_worker;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private static int splachtime = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        },splachtime);
+    }
+}

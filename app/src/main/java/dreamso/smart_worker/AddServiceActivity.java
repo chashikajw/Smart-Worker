92557fe99f38666519d401a2eef79fbeabb07395
chashikajw <chashikajw007@gmail.com>
2018-05-22 17:12:31 +0530

    modify menu

--- app/src/main/java/dreamso/smart_worker/AddServiceActivity.java
+++ app/src/main/java/dreamso/smart_worker/AddServiceActivity.java
@@ -1,12 +1,18 @@
 package dreamso.smart_worker;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,7 +23,7 @@ import com.google.firebase.database.DatabaseReference;
  * Created by admin on 5/20/2018.
  */
 
-public class AddServiceActivity extends AppCompatActivity {
+public class AddServiceActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     public static final String TAG = "AddServiceActivity";
 
@@ -26,6 +32,9 @@ public class AddServiceActivity extends AppCompatActivity {
 
     private static String categorySt, titleSt, mobileNumberSt, descriptionSt,
             addressSt;
+
+    ListView lst;
+    String categorylst[] = {"Category","Cleaning", "Shoping","Day care" ,"Repairing","Other"};
 
 
 
@@ -39,15 +48,43 @@ public class AddServiceActivity extends AppCompatActivity {
 
         Button btnSetLocation = (Button) findViewById(R.id.setlocationBtn);
 
-        category = (EditText) findViewById(R.id.category);
+        //category = (EditText) findViewById(R.id.category);
         title = (EditText) findViewById(R.id.title);
         mobileNumber = (EditText) findViewById(R.id.mobileNumber);
         description = (EditText)findViewById(R.id.description);
         address = (EditText) findViewById(R.id.address);
 
+        Spinner mSpinner = (Spinner) findViewById(R.id.spinner);
+        lst = (ListView) findViewById(R.id.lst_view);
+        ArrayAdapter<String> arrayaspter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,categorylst);
+        arrayaspter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(arrayaspter);
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
 
 
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                categorySt = selectedItem;
+                if(selectedItem.equals("Add new category"))
+                {
+                    // do your stuff
+                }
+            } // to close the onItemSelected
+            public void onNothingSelected(AdapterView<?> parent)
+            {
 
+            }
+        });
+
+
+        Toolbar mtoolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mtoolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 
 
@@ -55,7 +92,9 @@ public class AddServiceActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                categorySt = category.getText().toString();
+
+
+                //categorySt = category.getText().toString();
                 titleSt = title.getText().toString();
                 mobileNumberSt = mobileNumber.getText().toString();
                 descriptionSt = description.getText().toString();
@@ -73,6 +112,12 @@ public class AddServiceActivity extends AppCompatActivity {
 
             }
         });
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
 
     }
